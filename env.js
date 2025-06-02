9158b5d7345555091e0b38c917b88069a7eb50af
Kharay33455 <shakibkhan276337@gmail.com>
2025-06-02 08:51:51 +0100

    Updates

--- env.js
+++ env.js
@@ -70,7 +70,7 @@ window.env = {
   "REACT_APP_BH_DEV": "http://192.168.0.3:8000",
   "REACT_APP_BH_PROD": "https://yueh33455.pythonanywhere.com",
   "REACT_APP_COMPNAME": "Cashien",
-  "REACT_APP_ENV": "PROD",
+  "REACT_APP_ENV": "DEV",
   "REACT_APP_WS_DEV": "ws://192.168.0.3:8080",
   "REACT_APP_WS_PROD": "wss://outbound-wogo.onrender.com",
   "SESSIONNAME": "Console",
